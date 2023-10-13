af9f6282808bf0f0c8bd056ccef7a0ce7aac2a13
Mattis Bodynek <mattisbodynek@gmail.com>
2023-10-13 18:39:35 -0400

    Intermediate state: coloring the rows

--- expense_tracker/src/ExpenseTrackerApp.java
+++ expense_tracker/src/ExpenseTrackerApp.java
@@ -1,7 +1,16 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale.Category;
+import java.awt.Color;
 import javax.swing.JOptionPane;
+import java.awt.Component;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
 import controller.ExpenseTrackerController;
+import model.AmountFilter;
+import model.CategoryFilter;
 import model.ExpenseTrackerModel;
 import view.ExpenseTrackerView;
 import model.Transaction;
@@ -10,7 +19,7 @@ import controller.InputValidation;
 public class ExpenseTrackerApp {
 
   public static void main(String[] args) {
-    
+
     // Create MVC components
     ExpenseTrackerModel model = new ExpenseTrackerModel();
     ExpenseTrackerView view = new ExpenseTrackerView();
@@ -21,19 +30,40 @@ public class ExpenseTrackerApp {
 
     // Handle add transaction button clicks
     view.getAddTransactionBtn().addActionListener(e -> {
+      // Reset color renderer
+      view.colorTableRows(new LinkedList<Integer>(), null);
       // Get transaction data from view
       double amount = view.getAmountField();
       String category = view.getCategoryField();
-      
+
       // Call controller to add transaction
       boolean added = controller.addTransaction(amount, category);
-      
+
       if (!added) {
         JOptionPane.showMessageDialog(view, "Invalid amount or category entered");
         view.toFront();
       }
     });
 
-  }
+    view.getFilterTransactionBtn().addActionListener(e -> {
+      List<Transaction> transactions = model.getTransactions();
+      // Remove former filter operations
+      view.refreshTable(transactions);
+      // Get filtered transactions
+      List<Transaction> filtered = controller.applyFilter();
+      // Get indices of filtered rows
+      List<Integer> filteredIndices = new LinkedList<>();
 
+      for (int i = 0; i < transactions.size(); i++) {
+        for (Transaction t : filtered) {
+          if (t == transactions.get(i)) {
+            filteredIndices.add(i);
+          }
+        }
+      }
+
+      // Overwrite renderer for row colors
+      view.colorTableRows(filteredIndices, new Color(173, 255, 168));
+    });
+  }
 }
